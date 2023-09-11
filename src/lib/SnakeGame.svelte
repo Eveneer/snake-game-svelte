96be210c45c67c41ad4a6d5dcfabfab095b439a0
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-12 04:09:23 +0600

    Work on snake game board

---
+++ src/lib/SnakeGame.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const maxBoardSize: number = 500;
+	const boardPadding: number = 40;
+	let screenWidth: number;
+	let screenHeight: number;
+	let boardSize: number;
+
+	onMount(() => {
+		boardSize = Math.min(maxBoardSize, screenWidth - boardPadding);
+	});
+</script>
+
+<svelte:window bind:innerHeight={screenHeight} bind:innerWidth={screenWidth} />
+<div class="w-full max-w-[400px] bg-red-300 h-full min-h-[600px] flex flex-col p-5">
+	<div
+		class="relative mx-auto border border-black"
+		style="width: {boardSize}px; height: {boardSize}px"
+	>
+		Board
+	</div>
+</div>
