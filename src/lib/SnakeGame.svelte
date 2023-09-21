a86d8e2d431ec464d155d8fa5b254b9aa79feb6f
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-21 21:51:10 +0600

    Update snake game to use components and move global css to app.css

--- src/lib/SnakeGame.svelte
+++ src/lib/SnakeGame.svelte
@@ -2,7 +2,8 @@
 	import GCS from 'game-control-system/dist';
 	import { onMount } from 'svelte';
 	import GameBoard from './GameBoard.js';
-	import { boardDimension } from './constants.js';
+	import SnakeBodyPart from './SnakeBodyPart.svelte';
+	import FoodParticle from './FoodParticle.svelte';
 
 	export let maxBoardSize: number = 500;
 	export let boardPadding: number = 20;
@@ -16,8 +17,13 @@
 		boardSize = Math.min(maxBoardSize, screenWidth - boardPadding * 2);
 	});
 
-	const dimensionUnit = 100 / boardDimension;
-	console.log(board.snake.body);
+	// const control = new GCS({
+	// 	progression: 'time-based',
+	// 	speed: 300,
+	// 	loseCheckCallback: yourFunctionToCheckIfPlayerLost,
+	// 	gameStateCallback: yourFunctionToReturnGameState,
+	// 	gameStateProgressionCallback: yourFunctionToProgressTheGameState
+	// });
 </script>
 
 <svelte:window bind:innerHeight={screenHeight} bind:innerWidth={screenWidth} />
@@ -26,15 +32,16 @@
 	style="max-width: {maxBoardSize + boardPadding * 2}px; padding: {boardPadding}px"
 >
 	<div class="board" style="width: {boardSize}px; height: {boardSize}px">
-		{#each board.snake.body as part}
-			<span
-				class="absolute rounded-[25%] {part.classes === ''
-					? 'bg-white'
-					: `${part.classes}`} transform-gpu"
-				style="top: {part.position.row * dimensionUnit}%; left: {(part.position.col %
-					boardDimension) *
-					dimensionUnit}%; height: {dimensionUnit}%; width: {dimensionUnit}%"
+		{#each board.snake.body as part, index}
+			<SnakeBodyPart
+				{part}
+				partType={index === 0 ? 'head' : index === board.snake.body.length - 1 ? 'tail' : 'body'}
+				speed={300}
 			/>
+		{/each}
+
+		{#each board.foodParticles as particle}
+			<FoodParticle {particle} />
 		{/each}
 	</div>
 </div>
@@ -53,32 +60,6 @@
 		margin-right: auto;
 		border: 1px solid;
 		border-radius: 1%;
-		/* overflow: hidden; */
-	}
-
-	.transform-gpu {
-		transform: translate3d(var(--tw-translate-x), var(--tw-translate-y), 0) rotate(var(--tw-rotate))
-			skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x))
-			scaleY(var(--tw-scale-y));
-	}
-
-	.snake-head {
-		background-color: rgb(239, 68, 68);
-	}
-
-	.left {
-		border-radius: 25% 0% 0% 25% !important;
-	}
-
-	.right {
-		border-radius: 0% 25% 25% 0% !important;
-	}
-
-	.up {
-		border-radius: 25% 25% 0% 0% !important;
-	}
-
-	.down {
-		border-radius: 0% 0% 25% 25% !important;
+		overflow: hidden;
 	}
 </style>
