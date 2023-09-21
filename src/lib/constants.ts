17a0038b0f45a525a542402bd3d0d2fe671aad7e
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-21 20:16:52 +0600

    Update board dimension

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ import type { DirectionType } from './types.js';
 
 export const allDirections: DirectionType[] = ['left', 'right', 'up', 'down'];
 
-export const boardDimension: number = 20;
+export const boardDimension: number = 25;
