bf4c18d95191e4cfdb14cd5c5c1a4becb9018b04
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-22 01:05:10 +0600

    Fix food particle generation logic

--- src/lib/FoodParticleGenerationMixin.ts
+++ src/lib/FoodParticleGenerationMixin.ts
@@ -4,15 +4,17 @@ import type { FoodParticleType, SnakeBodyPartType, Constructor } from './types.j
 function FoodParticleGenerationMixin<TBase extends Constructor>(Base: TBase) {
 	return class extends Base {
 		generateFoodParticle: (snakeBody: SnakeBodyPartType[]) => FoodParticleType = (snakeBody) => {
-			let cardinality: number = Math.floor(Math.random() * 1600);
+			const maxCardinality = boardDimension * boardDimension;
+			let cardinality: number = Math.floor(Math.random() * maxCardinality);
 			let occupied: number[] = snakeBody.map((part) => part.cardinality);
 
-			while (occupied.includes(cardinality)) cardinality = Math.floor(Math.random() * 1600);
+			while (occupied.includes(cardinality))
+				cardinality = Math.floor(Math.random() * maxCardinality);
 
 			return {
 				position: {
-					row: Math.floor(cardinality / (boardDimension * 2)),
-					col: cardinality % (boardDimension * 2)
+					row: Math.floor(cardinality / boardDimension),
+					col: cardinality % boardDimension
 				},
 				cardinality,
 				points: Math.random() < 0.95 ? 5 : 10,
