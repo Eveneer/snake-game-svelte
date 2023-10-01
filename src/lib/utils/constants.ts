68431f508954b240b1a6bf92e6e9a38124d2e6b7
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-01 23:55:31 +0600

    Add new constants

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -2,7 +2,17 @@ import type { DirectionType } from '../types.js';
 
 export const allDirections: DirectionType[] = ['left', 'right', 'up', 'down'];
 
+export const leftDir: DirectionType = 'left'; // 0
+
+export const downDir: DirectionType = 'down'; // 1
+
+export const rightDir: DirectionType = 'right'; // 2
+
+export const upDir: DirectionType = 'up'; // 3
+
 export const boardDimension: number = 25;
+
+export const maxCoordinateValue: number = boardDimension - 1;
 
 export const defaultMaxBoardSize: number = 500;
 
