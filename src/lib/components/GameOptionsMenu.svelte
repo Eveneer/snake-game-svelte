22d7252ea685b05630d3d72a4260606a1c9b7bc2
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-04 04:10:38 +0600

    Work on game options menu

---
+++ src/lib/components/GameOptionsMenu.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type GameBoard from "$lib/utils/GameBoard.js";
+
+    export let board: GameBoard;
+
+    const options = [
+        {
+            name: "Reset game",
+            callback: () => {}
+        },
+        {
+            name: "Tutorial",
+            callback: () => {}
+        },
+    ];
+</script>
+
+<div
+	class="w-full bg-green-500 absolute z-10 top-0 left-0 transition-all !duration-[600ms]"
+	style="height: {board.control.getIsOptionsVisible() ? '100%' : '0%'};"
+/>
