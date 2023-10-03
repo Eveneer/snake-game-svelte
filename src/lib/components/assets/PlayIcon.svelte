c8b11307ec6dbc29bd8e590b909162e229c15a85
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-04 04:08:37 +0600

    Add game icons

---
+++ src/lib/components/assets/PlayIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let size: number = 48;
+	export let classes: string = 'fill-gray-600 dark:fill-gray-200';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 -960 960 960"
+	class="transit {classes}"
+	height={size}
+	width={size}
+>
+	<path
+		d="M381-239q-20 13-40.5 1.5T320-273v-414q0-24 20.5-35.5T381-721l326 207q18 12 18 34t-18 34L381-239Zm19-241Zm0 134 210-134-210-134v268Z"
+	/>
+</svg>
