ab1d96184d684394f76849a02a0efdbe73b62052
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-04 04:11:01 +0600

    Work on game start, pause and unpause

--- src/lib/components/SnakeGame.svelte
+++ src/lib/components/SnakeGame.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { defaultBoardPadding, defaultMaxBoardSize } from '$lib/utils/constants.js';
 	import GameBoard from '../utils/GameBoard.js';
 	import SnakeBodyPart from './SnakeBodyPart.svelte';
 	import FoodParticle from './FoodParticle.svelte';
-	import { defaultBoardPadding, defaultMaxBoardSize } from '$lib/utils/constants.js';
+	import IconButton from './buttons/IconButton.svelte';
+	import MenuIcon from './assets/MenuIcon.svelte';
+	import PlayIcon from './assets/PlayIcon.svelte';
+	import CloseIcon from './assets/CloseIcon.svelte';
+	import PauseIcon from './assets/PauseIcon.svelte';
 
 	export let maxBoardSize: number = defaultMaxBoardSize;
 	export let boardPadding: number = defaultBoardPadding;
@@ -12,19 +17,60 @@
 	let screenHeight: number;
 	let boardSize: number;
 	let board: GameBoard = new GameBoard();
-    let gamePlayInterval: ReturnType<typeof setInterval>;
+	let gamePlayInterval: ReturnType<typeof setInterval> | undefined;
+
+	$: menuIsShowing = board.control.getIsOptionsVisible();
+	$: gameIsRunnig = board.control.isRunning();
 
 	onMount(() => {
 		boardSize = Math.min(maxBoardSize, screenWidth - boardPadding * 2);
 	});
 
-	// const intervalFunc = setInterval(() => {
-	// 	if (!board.control.hasStarted()) {
-	// 		board.control.startGame();
-	// 	}
-	// 	board.control.progressGame();
-	// 	board = { ...board };
-	// }, board.control.getSpeed());
+	const toggleOptions: () => void = () => {
+		board.control.toggleOptionsVisibility();
+		board = { ...board };
+
+		if (board.control.getIsOptionsVisible()) {
+			pauseGame();
+		} else {
+			unpauseGame();
+		}
+	};
+
+	const gamePlayToggle: () => void = () => {
+		if (board.control.hasStarted()) {
+			toggleOptions();
+		} else {
+			board.control.startGame();
+			setGamePlayInterval();
+			board = { ...board };
+		}
+	};
+
+	const setGamePlayInterval = () => {
+		gamePlayInterval = setInterval(() => {
+			if (!board.control.hasStarted()) {
+				board.control.startGame();
+			}
+			board.control.progressGame();
+			board = { ...board };
+		}, board.control.getSpeed());
+	};
+
+	const pauseGame = () => {
+		board.control.pauseGame();
+		clearInterval(gamePlayInterval);
+		board = { ...board };
+	};
+
+	const unpauseGame = () => {
+		setTimeout(() => {
+			board.control.unpauseGame();
+			board = { ...board };
+			setGamePlayInterval();
+		}, 300);
+		board = { ...board };
+	};
 </script>
 
 <svelte:window bind:innerHeight={screenHeight} bind:innerWidth={screenWidth} />
@@ -32,6 +78,39 @@
 	class="board-container"
 	style="max-width: {maxBoardSize + boardPadding * 2}px; padding: {boardPadding}px"
 >
+	<div class="flex flex-row justify-between">
+		<div>
+			<IconButton onclickCallback={toggleOptions}>
+				<div
+					class="overflow-hidden transition-all !duration-[600ms]"
+					style="width: {menuIsShowing ? 0 : 24}px; height: {menuIsShowing ? 0 : 24}px;"
+				>
+					<MenuIcon size={24} />
+				</div>
+				<div
+					class="overflow-hidden transition-all !duration-[600ms]"
+					style="width: {!menuIsShowing ? 0 : 24}px; height: {!menuIsShowing ? 0 : 24}px;"
+				>
+					<CloseIcon size={24} />
+				</div>
+			</IconButton>
+			<IconButton onclickCallback={gamePlayToggle}>
+				<div
+					class="overflow-hidden transition-all !duration-[600ms]"
+					style="width: {gameIsRunnig ? 0 : 24}px; height: {gameIsRunnig ? 0 : 24}px;"
+				>
+					<PlayIcon size={24} />
+				</div>
+				<div
+					class="overflow-hidden transition-all !duration-[600ms]"
+					style="width: {!gameIsRunnig ? 0 : 24}px; height: {!gameIsRunnig ? 0 : 24}px;"
+				>
+					<PauseIcon size={24} />
+				</div>
+			</IconButton>
+		</div>
+		<div id="" />
+	</div>
 	<div class="board">
 		{#each board.snake.body as part, index (index)}
 			<SnakeBodyPart
@@ -44,6 +123,10 @@
 		{#each board.foodParticles as particle}
 			<FoodParticle {particle} />
 		{/each}
+		<div
+			class="w-full bg-green-500/10 absolute z-10 top-0 left-0 transition-all !duration-[600ms]"
+			style="height: {board.control.getIsOptionsVisible() ? '100%' : '0%'};"
+		/>
 	</div>
 </div>
 
@@ -53,6 +136,8 @@
 		height: 100%;
 		display: flex;
 		flex-direction: column;
+		gap: 1rem;
+		position: relative;
 	}
 
 	.board {
