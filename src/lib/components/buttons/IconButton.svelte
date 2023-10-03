42fcdd728404b6829e07e3034f6085077709087f
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-04 04:08:53 +0600

    Add game button components

---
+++ src/lib/components/buttons/IconButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let onclickCallback: () => void;
+	export let classes: string = '';
+	export let styling: string = '';
+</script>
+
+<button
+	type="button"
+	on:click={onclickCallback}
+	class="w-[40px] h-[40px] p-2 bg-white/20 hover:bg-white/30 transition-all rounded-md {classes}"
+	style={styling}
+>
+	<slot />
+</button>
